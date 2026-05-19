24dcfd1c6b1d464ba91293d2becb6aad5f135c15
Dominique <dominique.bitetti@gmail.com>
2026-05-19 22:08:44 +0200

    Generate Spring Boot backend

---
+++ backend/src/main/java/com/dommorgb/monstervault/MonsterVaultApplication.java
@@ -0,0 +1,13 @@
+package com.dommorgb.monstervault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsterVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonsterVaultApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dommorgb/monstervault/MonsterVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dommorgb.monstervault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonsterVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
